12ad1374b55de9116f46445226cfa6f7899de5d5
Dmitry <dmitryboremsky@gmail.com>
2019-08-11 22:48:07 +0300

    task01 - homework

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 "use strict";
-alert("hello world")
+
+let money = prompt("Ваш бюджет на месяц","00000"),
+    time = prompt ("Введите дату в формате","YYYY-MM-DD"),
+    appData = {
+        budget: money,
+        timeData: time,
+        expenses: { 
+            firstAnsw: prompt("Введите обязательную статью расходов в этом месяце","..."),
+            secondAnsw: prompt("Во сколько обойдется?","..."),
+        },
+        optionalExpenses: undefined,
+        income: [],
+        savings: false
+    };
+
+alert(appData.budget)
+
+
+console.log(appData.budget);
+console.log(appData.timeData);
+console.log(appData.expenses.firstAnsw);
+console.log(appData.expenses.secondAnsw)
